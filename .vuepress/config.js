a88e61195bcfed9f2cf825bb78e5df0f739f5c13
Vittorio Minacori <vittoriominacori@gmail.com>
2024-03-02 19:25:16 +0100

    fix: remove user scalable no

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -23,7 +23,7 @@ module.exports = {
       "meta",
       {
         name: "viewport",
-        content: "width=device-width, initial-scale=1, user-scalable=no",
+        content: "width=device-width, initial-scale=1",
       },
     ],
     ["meta", { property: "og:type", content: "website" }],
